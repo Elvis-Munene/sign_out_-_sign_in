49b4b479e1e7932f6af5ecc3f3eb136da42b7319
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-10-16 12:26:57 +0300

    creates a sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
-    
+    def new
+    end
+    def create 
+        user = User.find_by(email: params[:email])
+        #finds the existing user checks if the user can be authenticated.
+        if user.present? && user.authenticate(params[:password])
+            #sets up user.id sessions
+        session[:user_id] = user.id
+        redirect_to_root_path, notice: 'Logged in successfully'
+        else
+            flash.now[:alert] = 'Invalid email or password'
+            reder :new
+        end
+        def destroy
+            #deletes the user session
+            session[:user_id] = nil
+            redirect_to_root_path, notice: 'Logged Out'
+        end
+        
 end
